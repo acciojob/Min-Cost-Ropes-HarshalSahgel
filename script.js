42062b1e5388ab5e739e36323ea4d290c55290c9
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-27 18:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+    arr.sort((a,b) => a-b);
+	
+	let totalCost = 0;
+	while(arr.length > 1){
+		const firstElement = arr.shift();
+		const secondElement = arr.shift();
+
+		const newRope = firstElement + secondElement;
+			totalCost = totalCost+newRope;
+
+		  arr.push(newRope);
+
+		arr.sort((a,b) => a-b);
+		
+	}
+	return totalCost;
   
 }
 
